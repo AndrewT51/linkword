a92c61f04eb2741cd3c0b2101b983a7eabfc51a8
Andrew Trigg <andrew.trigg@kukd.com>
2016-12-28 16:47:01 +0000

    Swiper added

--- Components/OtherRoute.js
+++ Components/OtherRoute.js
@@ -1,6 +1,8 @@
 // import AnimalData from '../Data/Animals'
 import { Button } from './Button';
 import React, { Component } from 'react';
+import Swiper from 'react-native-swiper'
+
 import {
   StyleSheet,
   Text,
@@ -17,38 +19,68 @@ export class OtherRoute extends Component{
           <Text style={styles.welcome}>The Spanish word for {data[0].en} is {data[0].sp} {data[0].desc}</Text>
       )
     }
+    this.allSlides = function(){
+      return data.map(function(element){
+        <View style={styles.slide1}>
+          {this.content.bind(this)}
+        </View>
+      })
+    }
     console.log(this.props.route.data)
     return (
-      <View style={styles.container}
-        onStartShouldSetResponder = {function(evt){
-          console.log(evt.locationX)
-          return true
-        }}
-        onResponderGrant = {function(){console.log('aaaa')}}
-        onResponderMove = {function(){console.log('boooooo')}}
+      <Swiper style={styles.wrapper}
+        showsPagination={false}
+        horizontal={false}
+        // showsButtons={true}
       >
-        {this.content()}
-      </View>
+        <View style={styles.slide1}>
+          {this.allSlides.bind(this)}
+        </View>
+      </Swiper>
     )
   }
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+  wrapper: {
+    },
+    slide1: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#9DD6EB'
+    },
+    slide2: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#97CAE5',
+    },
+    slide3: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#92BBD9',
+    },
+    text: {
+      color: '#fff',
+      fontSize: 30,
+      fontWeight: 'bold',
+    }
 });
+
+
+
+// <View style={styles.slide1}>
+//           <Text style={styles.text}>Hello Swiper</Text>
+//         </View>
+        
+//         <View style={styles.slide2}>
+//           <Text style={styles.text}>Beautiful</Text>
+//         </View>
+        
+//         <View style={styles.slide3}>
+//           <Text style={styles.text}>And simple</Text>
+//         </View>
